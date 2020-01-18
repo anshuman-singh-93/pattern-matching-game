992f8381dee3b2fc989fa7b5bb77ea8c86a3b5e0
anshumanpbh@gmail.com <anshumanpbh@gmail.com>
2020-01-19 01:34:00 +0530

    cleaned unused code and code formatting

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let click_counter = 0;
 let win = 0;
 let loss = 0;
 
-function buildPattern(){
+function buildPattern() {
     let div = document.createElement('div');
     div.style.width = '200px';
     div.style.height = '200px';
@@ -19,33 +19,30 @@ function buildPattern(){
 
 }
 
-function assignColor(){
+function assignColor() {
 
     let bgColorCombination = [
-        ['red','green','#ffc107','blue'],
-        ['green','red','blue','#ffc107'],
-        ['blue','#ffc107','red','green'],
-        ['blue','red','green','#ffc107'],
-        ['red','green','blue','#ffc107'],
-        ['green','red','#ffc107','blue'],
-        ['blue','red','#ffc107','green']
+        ['red', 'green', '#ffc107', 'blue'],
+        ['green', 'red', 'blue', '#ffc107'],
+        ['blue', '#ffc107', 'red', 'green'],
+        ['blue', 'red', 'green', '#ffc107'],
+        ['red', 'green', 'blue', '#ffc107'],
+        ['green', 'red', '#ffc107', 'blue'],
+        ['blue', 'red', '#ffc107', 'green']
 
     ]
 
-    let randomBgColorIndex = Math.floor(Math.random()* bgColorCombination.length)
+    let randomBgColorIndex = Math.floor(Math.random() * bgColorCombination.length)
 
-    patterns.forEach((div,i)=>{
+    patterns.forEach((div, i) => {
         div.style.backgroundColor = bgColorCombination[randomBgColorIndex][i];
-        div.setAttribute('id',i)
+        div.setAttribute('id', i)
         game_node.appendChild(div)
     })
 }
 
-function init(){
-  
-}
 
-function initialize (){
+function initialize() {
     game_node.innerHTML = '';
     play_btn.removeAttribute('disabled');
     patterns = [];
@@ -56,76 +53,72 @@ function initialize (){
 }
 
 
-function buildGame (e){
+function buildGame(e) {
 
     initialize();
 
-    for(let i = 1 ; i<=4 ; i++){
+    for (let i = 1; i <= 4; i++) {
         patterns.push(buildPattern());
     }
 
-
     assignColor()
     showResult();
-
-
-
 }
 
-function showResult(){
+function showResult() {
     win_node.innerHTML = win;
     loss_node.innerHTML = loss;
 }
-function onPatternClick (e){
-    let id  =  this.getAttribute('id')
+function onPatternClick(e) {
+    let id = this.getAttribute('id')
 
-    if(highlightedPatterns[click_counter].getAttribute('id') === id){
+    if (highlightedPatterns[click_counter].getAttribute('id') === id) {
         click_counter++;
-    }else{
+    } else {
         loss++
         return buildGame();
     }
-    if(click_counter === 3){
+    if (click_counter === 3) {
         win++;
         return buildGame();
     }
 }
 
-function wait(){
-    return new Promise((resolve,reject)=>{
-        setTimeout(resolve,600)
+function wait() {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, 600)
     })
 }
-async function startPatternHighlighting (){
+async function startPatternHighlighting() {
     let patternCombination = [
-        [0,1,2],
-        [1,2,0],
-        [0,2,1],
-        [2,1,0],
-        [2,0,1]
+        [0, 1, 2],
+        [1, 2, 0],
+        [0, 2, 1],
+        [2, 1, 0],
+        [2, 0, 1]
     ]
-    let randomIndex = Math.floor(Math.random()* patternCombination.length)
-    for(let v of patternCombination[randomIndex] ){
+    let randomIndex = Math.floor(Math.random() * patternCombination.length)
+    for (let v of patternCombination[randomIndex]) {
         patterns[v].style.opacity = 1
         await wait()
         patterns[v].style.opacity = 0.5
         highlightedPatterns.push(patterns[v])
 
     }
-   
+
 
 }
 
-function onPlayStarted(e){
+function onPlayStarted(e) {
     hasGameStarted = true;
-    play_btn.setAttribute('disabled',1)
+    play_btn.setAttribute('disabled', 1)
 
     startPatternHighlighting()
 
-    patterns.forEach((div)=>{
+    patterns.forEach((div) => {
         div.addEventListener('click', onPatternClick)
     })
 }
 
 window.addEventListener('DOMContentLoaded', buildGame);
-play_btn.addEventListener('click',onPlayStarted);
+play_btn.addEventListener('click', onPlayStarted);
